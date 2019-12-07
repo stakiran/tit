56a20047fb7d1c0d221184bb71ef9d48c4b866c2
stakiran <stakiran@users.noreply.github.com>
2019-12-07 20:13:47 +0900

    change n.bat to day prefixing

--- n.bat
+++ n.bat
@@ -4,13 +4,14 @@ rem yyyy/mm/dd
 set datebase=%date%
 set y4=%datebase:~0,4%
 set mo2=%datebase:~5,2%
+set d2=%datebase:~8,2%
 
 mkdir %~dp0%y4% > nul 2>&1
 mkdir %~dp0%y4%\%mo2% > nul 2>&1
 
 set /p yourcomment="Filename?(Need .md extension!!) >>>";
 
-set fullpath=%~dp0%y4%\%mo2%\%yourcomment%
+set fullpath=%~dp0%y4%\%mo2%\%d2%_%yourcomment%
 
 copy nul %fullpath%
 
